bc65fad0266e8e5e352720d63157c222c3dc20d4
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2024-02-11 00:47:17 +0200

    make speech function

---
+++ AI-assistant.py
@@ -0,0 +1,15 @@
+import pyttsx3
+
+def speech(text):
+    engine=pyttsx3.init()
+    voices=engine.getProperty("voices")
+    engine.setProperty('voice',voices[1].id)
+    engine.setProperty('rate',120)
+    engine.say(text)
+    engine.runAndWait()
+    
+    
+    
+    
+    
+speech("hello world")
