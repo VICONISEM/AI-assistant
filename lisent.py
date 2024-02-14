0905fd226d50bee521c0060557266edb8e48befe
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2024-02-14 21:01:16 +0200

    update function of recognition

--- lisent.py
+++ lisent.py
@@ -1,10 +1,43 @@
 import speech_recognition as sr
-import speech
+import speak_module
+from time import sleep
+from speech_recognition import UnknownValueError
 
-r=sr.Recognizer()
-source=sr.Microphone(0)
-with source as mic:
-    audio=r.listen(mic)
-text=r.recognize_google(audio) 
-speech.speech(text)
+
+
+# global variables
+r = sr.Recognizer()
+source = sr.Microphone(0)
+keywords = [("tito", 1), ("wakeup tito", 1)]
+
+
+def start_reco():
+    speak_module.speak("tell me how i can help you to day ")
+    r=sr.Recognizer()
+    with sr.Microphone(0) as source:
+        audio=r.listen(source)
+    text=r.recognize_google(audio)
+    speak_module.speak(text)
+    
+    
+    
+def callback(recognizer, audio):
+    try:
+        text = recognizer.recognize_sphinx(audio, keyword_entries=keywords)
+        if ("tito" in text or "wakeup tito" ):
+            speak_module.speak("yes sir")
+            start_reco()
+            
+    except UnknownValueError as e:
+        print("Couldn't understand the audio. Please try again.")
+
+
+
+def start_recognize():
+    print("listen........")
+    r.listen_in_background(source, callback)
+    sleep(10)
+
+
+start_recognize()
 
--- speak_module.py
+++ speak_module.py
@@ -1,9 +1,9 @@
 import pyttsx3
 
-def speech(text):
+def speak(text):
     tts_engine=pyttsx3.init()
     voices=tts_engine.getProperty("voices")
-    tts_engine.setProperty('voice',voices[1].id)
+    tts_engine.setProperty('voice',voices[0].id)
     tts_engine.setProperty('rate',120)
     tts_engine.say(text)
     tts_engine.runAndWait()
