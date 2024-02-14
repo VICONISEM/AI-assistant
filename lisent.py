0eeb99fb227446df8da240d44145011668e56aff
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2024-02-14 21:10:26 +0200

    test speak

--- lisent.py
+++ lisent.py
@@ -8,10 +8,10 @@ from speech_recognition import UnknownValueError
 # global variables
 r = sr.Recognizer()
 source = sr.Microphone(0)
-keywords = [("tito", 1), ("wakeup tito", 1)]
+keywords = [("tito", 0.7), ("wakeup tito", 1)]
 
 
-def start_reco():
+def start_main():
     speak_module.speak("tell me how i can help you to day ")
     r=sr.Recognizer()
     with sr.Microphone(0) as source:
@@ -19,24 +19,21 @@ def start_reco():
     text=r.recognize_google(audio)
     speak_module.speak(text)
     
-    
-    
-def callback(recognizer, audio):
-    try:
-        text = recognizer.recognize_sphinx(audio, keyword_entries=keywords)
-        if ("tito" in text or "wakeup tito" ):
-            speak_module.speak("yes sir")
-            start_reco()
-            
-    except UnknownValueError as e:
-        print("Couldn't understand the audio. Please try again.")
-
 
 
 def start_recognize():
-    print("listen........")
-    r.listen_in_background(source, callback)
-    sleep(10)
+    print("listen ........")
+    with source:
+       audio= r.listen(source)
+    try:
+        text = r.recognize_sphinx(audio, keyword_entries=keywords)
+        if ("tito" in text or "wakeup tito" ):
+            speak_module.speak("yes sir")
+            start_main()
+        
+    except UnknownValueError as e:
+     print("Couldn't understand the audio. Please try again.")
+
 
 
 start_recognize()
